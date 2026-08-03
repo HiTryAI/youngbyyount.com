e4727e85c2c1c3b8e4518a0695a419147b6c5bd0
Benjamin Yount <HiTryAI@users.noreply.github.com>
2026-08-03 23:38:20 +0300

    Browser-Duell: optionaler Blick auf die Runde des Herausforderers

--- d/play/flutter_bootstrap.js
+++ d/play/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "726362562" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4154426239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
