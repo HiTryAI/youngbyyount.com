982327459415ac70999fc88738f92c45d3ec71cc
Benjamin Yount <HiTryAI@users.noreply.github.com>
2026-08-04 12:54:41 +0300

    Browser-Duell: kein Nochmal mehr – eine Runde pro Link, weiter geht es in der App

--- d/play/flutter_bootstrap.js
+++ d/play/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=8d2181d16a"},{}]};
+_flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=dfc76ac7a9"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4154426239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "294402554" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
