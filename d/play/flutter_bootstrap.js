80b25bacab612b876898940fc5b8cb685e0a3a9c
Benjamin Yount <HiTryAI@users.noreply.github.com>
2026-08-16 18:33:26 +0300

    Kopfrechnen-Duell und Gleisplan – auch im Browser
    Zwei neue Türen (/g/chalkboard/, /g/tracks/), zwei Kacheln in der Galerie,
    alle Zählstellen auf 64. Browser-Fassung neu gebaut und gestempelt.

--- d/play/flutter_bootstrap.js
+++ d/play/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=3d2b7fe3c3"},{}]};
+_flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=37beb4a701"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2049630641" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1493818150" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
